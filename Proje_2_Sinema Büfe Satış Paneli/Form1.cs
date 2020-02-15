739e90e521fc0e80a316e2ccb3ffb5090bd6fa76
mstfyrdmc <45285832+AdamGiby@users.noreply.github.com>
2020-02-15 19:05:34 +0300

    SinemaBufe

---
+++ Proje_2_Sinema Büfe Satış Paneli/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje_2_Sinema_Büfe_Satış_Paneli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int kasatutar = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cay, misir, bilet, su, toplam;
+            misir = Convert.ToInt16(textBox1.Text);
+            su = Convert.ToInt16(textBox2.Text);
+            cay = Convert.ToInt16(textBox3.Text);
+            bilet = Convert.ToInt16(textBox4.Text);
+
+            toplam = misir * 4 + su * 1 + bilet * 8 + cay * 2;
+            kasatutar = kasatutar + toplam;
+            label10.Text = toplam.ToString() + " TL ";
+            label12.Text = kasatutar.ToString() + "TL";
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
